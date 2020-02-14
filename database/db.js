db2716686954bccffa5bbe2afb54883c56e84117
Ahsan Zia <ahsan97@hotmail.co.uk>
2020-02-14 17:32:54 +0000

    registration/login works

--- database/db.js
+++ database/db.js
@@ -1,16 +1,16 @@
 const Sequelize = require('sequelize')
 const db = {}
 const sequelize = new Sequelize('test_login', 'root', '123123', {
-  host: 'localhost',
-  dialect: 'mysql',
-  operatorsAliases: false,
+    host: 'localhost',
+    dialect: 'mysql',
+    operatorsAliases: false,
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 })
 
 db.sequelize = sequelize

29f8d4745d70f4d93d8db36843a68bfe45d25d57
Ahsan Zia <ahsan97@hotmail.co.uk>
2020-02-15 14:35:16 +0000

    basic news page setup

--- client/src/components/news.js
+++ client/src/components/news.js
@@ -3,20 +3,54 @@ import React, { Component } from 'react'
 class News extends Component {
     constructor(props) {
         super(props)
-    
+
         this.state = {
-             
+            headline: undefined,
+            body: undefined,
+            image: undefined
         }
     }
 
+    FetchDataFromRssFeed() {
+        const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
+
+        let Parser = require('rss-parser');
+        let parser = new Parser();
+        parser.parseURL(CORS_PROXY + 'http://feeds.bbci.co.uk/news/rss.xml', function (err, feed) {
+            if (err) throw err;
+            console.log(feed.items[0].contentSnippet);
+            this.setState({
+                headline: feed.items[0].title,
+                body: feed.items[0].contentSnippet
+            })
+            /*feed.items.forEach(function (entry) {
+                console.log(entry.title + ':' + entry.link);
+            })*/
+        })
+
+    }
+
+    componentDidMount() {
+        const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
+
+        let Parser = require('rss-parser');
+        let parser = new Parser();
+        parser.parseURL(CORS_PROXY + 'http://feeds.bbci.co.uk/news/rss.xml').then((feed) =>{
+            this.setState({
+                headline: feed.items[0].title,
+                body: feed.items[0].contentSnippet
+            })
+        })
+    }
     render() {
         return (
             <div>
-                <p>Hi</p>
+                <p>{this.state.headline}</p>
+                <p>{this.state.body}</p>
             </div>
         )
     }
-    
+
 }
 
 export default News

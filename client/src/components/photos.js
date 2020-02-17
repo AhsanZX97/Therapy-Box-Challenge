7952fc830e4933ebabfcb66d007d00015f8c4578
Ahsan Zia <ahsan97@hotmail.co.uk>
2020-02-17 13:23:50 +0000

    worked on photo page

--- client/src/components/photos.js
+++ client/src/components/photos.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react'
+import add from './Add_picture.png'
 
 export default class photos extends Component {
 
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            pictures: []
+        }
+    }
+
+
     fileSelectHandler(e) {
-        
+
     }
 
     render() {
         return (
             <div>
-                <input type="file" onChange={this.fileSelectHandler}></input>
+                <img id="image1" src={add} alt="your image" />
+                <br></br>
+                <img id="image1" src={add} alt="your image" />
+                <br></br>
+                <img id="image1" src={add} alt="your image" />
+                <br></br>
+                <img id="image1" src={add} alt="your image" />
+                <br></br>
+                <img id="image1" src={add} alt="your image" />
+                <br></br>
+                <img id="image1" src={add} alt="your image" />
             </div>
         )
     }
